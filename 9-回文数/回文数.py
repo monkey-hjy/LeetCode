1d684136d05ab9dc08a3214bf7ffa6d2d8717906
monkey-hjy <50664775+monkey-hjy@users.noreply.github.com>
2020-04-01 16:40:23 +0800

    回文数

---
+++ 9-回文数/回文数.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/14 21:24
+# @Author  : Monkey
+# @File    : ClassCreate.py
+# @Software: PyCharm
+# @Demand  : 
+'''
+判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+'''
+
