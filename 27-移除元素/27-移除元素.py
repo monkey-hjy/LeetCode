003b8f1522b6b039ffff34c73b8293611b556dfa
monkey-hjy <50664775+monkey-hjy@users.noreply.github.com>
2020-04-04 15:00:03 +0800

    移除元素

---
+++ 27-移除元素/27-移除元素.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/4 14:58
+# @Author  : Monkey
+# @File    : 27-移除元素.py
+# @Software: PyCharm
+# @Demand  :
+'''
+给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == val:
+                count += 1
+                continue
+            nums[i-count] = nums[i]
+        return len(nums)-count
